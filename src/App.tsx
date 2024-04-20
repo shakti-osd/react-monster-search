bbeb3bdef549afb50585b522b5ea816c727bfaf0
Shakti Kumar <shakti.opensourcedeveloper@gmail.com>
2024-04-20 15:19:54 +0530

    Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,39 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import Header from '../src/components/Header'
-import SearchBox from '../src/components/SearchBox'
-import SortMonster from '../src/components/SortMonster'
-import MonsterList from '../src/components/MonsterList'
-import Footer from '../src/components/Footer'
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
+import { useState, useEffect } from "react"
+import axios from "axios"
+import Header from "../src/components/Header"
+import SearchBox from "../src/components/SearchBox"
+import SortMonster from "../src/components/SortMonster"
+import MonsterList from "../src/components/MonsterList"
+import Footer from "../src/components/Footer"
+import Box from "@mui/material/Box"
+import Container from "@mui/material/Container"
 
-import { SelectChangeEvent } from '@mui/material/Select';
-
-type InputData = {
-  timer: Number,
-  fn: Function,
-  d: Number,
-  e: Event
-}
+import { SelectChangeEvent } from "@mui/material/Select"
 
 function App() {
+  const [monsters, setMonsters] = useState([])
 
-
-  const [monsters, setMonsters] = useState([{}])
-  
   // Sort Order Monster
-  const [order, setOrder] = useState('');
+  const [order, setOrder] = useState("")
   const handleChangeOrder = (event: SelectChangeEvent) => {
-    setOrder(event.target.value as string);
-  };
+    setOrder(event.target.value as string)
+  }
 
   // End Sort Order Monster
 
   // Search Monster
-  const [search, setSearch] = useState<string>('')
+  const [search, setSearch] = useState<string>("")
 
-  const handleChange = (e: React.SyntheticEvent) => {
-    console.log(e.target.value)
+  const handleChange = (e: any) => {
     setSearch(e.target.value)
   }
 
-  const debaunce = function (fn:Function, d: number) {
-    let timer:number
+  const debaunce = function (fn: Function, d: number) {
+    let timer: number
     return function () {
-      let context = this, args = arguments;
-      console.log('args', args, context)
+      let context = this
+      let args = arguments
+      console.log("args", args, context)
       clearTimeout(timer)
       timer = setTimeout(() => {
         fn.apply(context, args)
@@ -51,56 +42,62 @@ function App() {
   }
 
   const searchMonster = debaunce(handleChange, 500)
-  
+
   // End Search Monster
-  
 
   useEffect(() => {
-    const monsterUserData = axios('https://jsonplaceholder.typicode.com/users')
-    monsterUserData.then(response => {
+    const monsterUserData = axios("https://jsonplaceholder.typicode.com/users")
+    monsterUserData.then((response) => {
       setMonsters(response.data)
-      
     })
-
   }, [])
-  
-  const monsterFilter = monsters.filter(item => item?.name?.toLowerCase().includes(search.toLowerCase()))
-  const monsterSorted =  monsterFilter.sort((a, b) => {
-      const nameA = a?.name.toUpperCase(); // ignore upper and lowercase
-      const nameB = b?.name.toUpperCase(); // ignore upper and lowercase
-    if (order === 'asc') {
+
+  const monsterFilter = monsters.filter((item: any) =>
+    item?.name?.toLowerCase().includes(search.toLowerCase())
+  )
+  const monsterSorted = monsterFilter.sort((a: any, b: any) => {
+    const nameA = a.name.toUpperCase() // ignore upper and lowercase
+    const nameB = b.name.toUpperCase() // ignore upper and lowercase
+    if (order === "asc") {
       if (nameA < nameB) {
-        return -1;
+        return -1
       }
       if (nameA > nameB) {
-        return 1;
+        return 1
       }
-    } else{
+    } else {
       if (nameA > nameB) {
-        return -1;
+        return -1
       }
       if (nameA < nameB) {
-        return 1;
+        return 1
       }
     }
 
-      // names must be equal
-      return 0;
-    });
-  
-  console.log('monsterSorted',monsterSorted, order)
+    // names must be equal
+    return 0
+  })
 
   return (
     <>
       <Header />
-        <Container sx={{ padding: { xs: '0 16px' } }}>
-          <Box sx={{ bgcolor: {xl:'#6b6464', xs: '#c6c6c6' }, height: 'auto', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }} >
+      <Container sx={{ padding: { xs: "0 16px" } }}>
+        <Box
+          sx={{
+            bgcolor: { xl: "#6b6464", xs: "#c6c6c6" },
+            height: "auto",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
           <SearchBox changeHandler={searchMonster} />
           <SortMonster changeOrder={handleChangeOrder} selectedOrder={order} />
-          </Box>
+        </Box>
       </Container>
       <MonsterList monsterData={monsterSorted} />
-        <Footer /> 
+      <Footer />
     </>
   )
 }
